9956bdf63dc25333e3a146740cee0d1872a01a54
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-08-02 19:21:52 +0530

    Add new Build

--- app.js
+++ app.js
@@ -12,12 +12,20 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get(("/"), (req, res) => {
-   res.send("<h1> Working Fine </h1>")
-})
 // app.get(("/"), (req, res) => {
 //    res.send("<h1> Working Fine </h1>")
 // })
+
+
+// Deploy Only
+import path from "path";
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "./client/build")))    // deploy only
+
+app.get('/', async (req, res) => {
+   res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 
 // Importing Routes
